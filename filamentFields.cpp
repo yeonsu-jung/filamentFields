4a89417dddea55fcf38519cccdcad936f90be65f
Yeonsu Jung <ynsuju@gmail.com>
2024-06-01 05:03:31 -0400

    use different formula for linking number

--- filamentFields.cpp
+++ filamentFields.cpp
@@ -1,9 +1,11 @@
 #define PYBIND11_DETAILED_ERROR_MESSAGES
+#define INV_TWO_PI                                                             \
+  (0.1591549430918953357688837633725143620344596457404564487476673440)
 
 #include "filamentFields.h"
 #include <iostream>
 #include <algorithm>
-
+// #include <cmath>
 
 filamentFields::filamentFields(const std::vector<Eigen::MatrixXd>& filament_nodes_list) :
     filament_nodes_list(filament_nodes_list)
@@ -40,7 +42,7 @@ filamentFields::filamentFields(const std::vector<Eigen::MatrixXd>& filament_node
     // compute_total_linking_matrix();
 }
 
-void filamentFields::updateFilamentNodesList(const std::vector<Eigen::MatrixXd>& _filament_nodes_list)
+void filamentFields::update_filament_nodes_list(const std::vector<Eigen::MatrixXd>& _filament_nodes_list)
 {
     filament_nodes_list = _filament_nodes_list;
     total_entanglement = 0;
@@ -55,7 +57,7 @@ void filamentFields::updateFilamentNodesList(const std::vector<Eigen::MatrixXd>&
     // compute_total_linking_matrix();
 }
 
-void filamentFields::updateContactArray(const Eigen::MatrixXd& _contact_array)
+void filamentFields::update_contact_array(const Eigen::MatrixXd& _contact_array)
 {
     contact_array = _contact_array;
     number_of_total_contacts = contact_array.rows();
@@ -118,7 +120,6 @@ void filamentFields::get_edge_pairs(double R_omega) {
     edge_pairs.reserve(all_edges.rows());
 
     edge_lengths = Eigen::VectorXd::Zero(all_edges.rows());
-    // precompute sphere container
     for (int idx = 0; idx < all_edges.rows(); ++idx) {
         Eigen::Vector3d edge1_start = all_edges.row(idx).segment<3>(0);
         Eigen::Vector3d edge1_end = all_edges.row(idx).segment<3>(3);
@@ -151,11 +152,8 @@ void filamentFields::precompute(double R_omega) {
     }
 
     get_edge_pairs(R_omega); // comes first to compute edge pairs
-
     compute_total_linking_matrix();
-
     compute_all_Q_tensors();
-
     is_precomputed = true;
 }
 
@@ -220,7 +218,7 @@ Eigen::VectorXi filamentFields::sample_edges_locally(const Eigen::Vector3d& quer
     return local_edge_trues;
 }
 
-Eigen::MatrixXd filamentFields::analyzeLocalVolume(const Eigen::Vector3d& query_point, double R_omega, double rod_radius) {
+Eigen::MatrixXd filamentFields::analyze_local_volume(const Eigen::Vector3d& query_point, double R_omega, double rod_radius) {
     // Sample edges locally
     Eigen::VectorXi local_edge_trues = sample_edges_locally(query_point, R_omega);
     number_of_labels = 0;
@@ -230,7 +228,7 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolume(const Eigen::Vector3d& query_
         number_of_labels = 0;
         volume_fraction = std::numeric_limits<double>::quiet_NaN();
         orientational_order_parameter = std::numeric_limits<double>::quiet_NaN();
-        entanglement = std::numeric_limits<double>::quiet_NaN();
+        local_entanglement = std::numeric_limits<double>::quiet_NaN();
         return Eigen::MatrixXd(0, 6);
     }
 
@@ -300,7 +298,7 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolume(const Eigen::Vector3d& query_
     //     return std::isnan(x) ? 0.0 : std::abs(x);
     // }).sum();
 
-    entanglement = entanglement_matrix.unaryExpr([](double x) -> double {
+    local_entanglement = entanglement_matrix.unaryExpr([](double x) -> double {
         return std::isnan(x) ? 0.0 : std::abs(x);
     }).sum();
 
@@ -327,7 +325,7 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolume(const Eigen::Vector3d& query_
     return local_edges;
 }
 
-Eigen::MatrixXd filamentFields::analyzeLocalVolumeFromPrecomputed(const Eigen::Vector3d& query_point, double R_omega, double rod_radius) {
+Eigen::MatrixXd filamentFields::analyze_local_volume_from_precomputed(const Eigen::Vector3d& query_point, double R_omega, double rod_radius) {
     // make sure that the total_linking_matrix is computed
     if (is_precomputed == false) {
         precompute(R_omega);
@@ -342,7 +340,7 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolumeFromPrecomputed(const Eigen::V
         number_of_labels = 0;
         volume_fraction = std::numeric_limits<double>::quiet_NaN();
         orientational_order_parameter = std::numeric_limits<double>::quiet_NaN();
-        entanglement = std::numeric_limits<double>::quiet_NaN();
+        local_entanglement = std::numeric_limits<double>::quiet_NaN();
         return Eigen::MatrixXd(0, 6);
     }
 
@@ -398,9 +396,9 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolumeFromPrecomputed(const Eigen::V
 
     for (int idx = 0; idx < local_edge_count; ++idx) {
         for (int jdx = idx+1; jdx < local_edge_count; ++jdx) {
-            // if (local_edge_labels(idx) == local_edge_labels(jdx)) {
-            //     continue;
-            // }
+            if (local_edge_labels(idx) == local_edge_labels(jdx)) {
+                continue;
+            }
 
             double debug1 = local_edge_indices(idx);
             double debug2 = local_edge_indices(jdx);
@@ -412,7 +410,7 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolumeFromPrecomputed(const Eigen::V
         }
     }
     
-    entanglement = entanglement_matrix.unaryExpr([](double x) -> double {
+    local_entanglement = entanglement_matrix.unaryExpr([](double x) -> double {
         return std::isnan(x) ? 0.0 : std::abs(x);
     }).sum();
 
@@ -438,37 +436,69 @@ Eigen::MatrixXd filamentFields::analyzeLocalVolumeFromPrecomputed(const Eigen::V
     return local_edges;
 }
 
+// Eigen::MatrixXd filamentFields::analyze_local_volume_over_domain(const Eigen::MatrixX3d& query_points, double R_omega, double rod_radius) {
+//     Eigen::MatrixXd results(query_points.rows(), 6);
+//     for (int i = 0; i < query_points.rows(); ++i) {
+//         Eigen::Vector3d query_point = query_points.row(i);
+//         analyze_local_volume_from_precomputed(query_point, R_omega, rod_radius);
+//         results.row(i) << number_of_labels, volume_fraction, orientational_order_parameter, local_entanglement, number_of_local_contacts, force_sum;
+//     }
+//     return results;
+// }
+
+Eigen::MatrixXd filamentFields::analyze_local_volume_over_domain(const Eigen::MatrixX3d& query_points, double R_omega, double rod_radius) {
+    Eigen::Matrix<double, Eigen::Dynamic, 6, Eigen::RowMajor> results(query_points.rows(), 6);
+    for (int i = 0; i < query_points.rows(); ++i) {
+        Eigen::Vector3d query_point = query_points.row(i);
+        analyze_local_volume_from_precomputed(query_point, R_omega, rod_radius);
+        results.row(i) << number_of_labels, volume_fraction, orientational_order_parameter, local_entanglement, number_of_local_contacts, force_sum;
+    }
+    return results;
+}
+
 
 double filamentFields::_clip(double x, double lower, double upper) const {
     return std::min(std::max(x, lower), upper);
 }
 
 double filamentFields::compute_linking_number_for_edges(const Eigen::VectorXd& e_i, const Eigen::VectorXd& e_j) const {
-    Eigen::Vector3d r_ij = e_i.segment<3>(0) - e_j.segment<3>(0);
-    Eigen::Vector3d r_ijj = e_i.segment<3>(0) - e_j.segment<3>(3);
-    Eigen::Vector3d r_iij = e_i.segment<3>(3) - e_j.segment<3>(0);
-    Eigen::Vector3d r_iijj = e_i.segment<3>(3) - e_j.segment<3>(3);
+    // Panagioutu and Kauffmand (Banchoff)
+    // Eigen::Vector3d r_ij = e_i.segment<3>(0) - e_j.segment<3>(0);
+    // Eigen::Vector3d r_ijj = e_i.segment<3>(0) - e_j.segment<3>(3);
+    // Eigen::Vector3d r_iij = e_i.segment<3>(3) - e_j.segment<3>(0);
+    // Eigen::Vector3d r_iijj = e_i.segment<3>(3) - e_j.segment<3>(3);
 
-    double tol = 1e-6;
+    // double tol = 1e-6;
     
-    Eigen::Vector3d n1 = r_ij.cross(r_ijj);
-    n1 /= (n1.norm() + tol);
+    // Eigen::Vector3d n1 = r_ij.cross(r_ijj);
+    // n1 /= (n1.norm() + tol);
     
-    Eigen::Vector3d n2 = r_ijj.cross(r_iijj);
-    n2 /= (n2.norm() + tol);
+    // Eigen::Vector3d n2 = r_ijj.cross(r_iijj);
+    // n2 /= (n2.norm() + tol);
     
-    Eigen::Vector3d n3 = r_iijj.cross(r_iij);
-    n3 /= (n3.norm() + tol);
+    // Eigen::Vector3d n3 = r_iijj.cross(r_iij);
+    // n3 /= (n3.norm() + tol);
     
-    Eigen::Vector3d n4 = r_iij.cross(r_ij);
-    n4 /= (n4.norm() + tol);
+    // Eigen::Vector3d n4 = r_iij.cross(r_ij);
+    // n4 /= (n4.norm() + tol);
 
-    return -1.0 / (4 * M_PI) * std::abs(
-        std::asin(filamentFields::_clip(n1.dot(n2), -1.0 + tol, 1.0 - tol)) +
-        std::asin(filamentFields::_clip(n2.dot(n3), -1.0 + tol, 1.0 - tol)) +
-        std::asin(filamentFields::_clip(n3.dot(n4), -1.0 + tol, 1.0 - tol)) +
-        std::asin(filamentFields::_clip(n4.dot(n1), -1.0 + tol, 1.0 - tol))
-    );
+    // return -1.0 / (4 * M_PI) * std::abs(
+    //     std::asin(filamentFields::_clip(n1.dot(n2), -1.0 + tol, 1.0 - tol)) +
+    //     std::asin(filamentFields::_clip(n2.dot(n3), -1.0 + tol, 1.0 - tol)) +
+    //     std::asin(filamentFields::_clip(n3.dot(n4), -1.0 + tol, 1.0 - tol)) +
+    //     std::asin(filamentFields::_clip(n4.dot(n1), -1.0 + tol, 1.0 - tol))
+    // );
+
+    // Qu and James (Arai)
+    Eigen::Vector3d a = e_i.segment<3>(0) - e_j.segment<3>(0);
+    Eigen::Vector3d b = e_i.segment<3>(0) - e_j.segment<3>(3);
+    Eigen::Vector3d c = e_i.segment<3>(3) - e_j.segment<3>(3);
+    Eigen::Vector3d d = e_i.segment<3>(3) - e_j.segment<3>(0);
+
+    double lk_ij = INV_TWO_PI * (std::atan2( a.dot(b.cross(c)),(a.norm() * b.norm() * c.norm() + a.dot(b) * c.norm() + c.dot(a)*b.norm() + b.dot(c)*a.norm()) ) +
+                    std::atan2( c.dot(d.cross(a)),(c.norm() * d.norm() * a.norm() + c.dot(d) * a.norm() + a.dot(c)*d.norm() + d.dot(a)*c.norm()) ));
+
+    return lk_ij;
 }
 
 
--- filamentFields.h
+++ filamentFields.h
@@ -10,8 +10,8 @@ public:
     filamentFields(const std::vector<Eigen::MatrixXd>& filament_nodes_list);
     filamentFields(const std::vector<Eigen::MatrixXd>& filament_nodes_list, const Eigen::MatrixXd& contact_array);
 
-    void updateFilamentNodesList(const std::vector<Eigen::MatrixXd>& _filament_nodes_list);
-    void updateContactArray(const Eigen::MatrixXd& _contact_array);
+    void update_filament_nodes_list(const std::vector<Eigen::MatrixXd>& _filament_nodes_list);
+    void update_contact_array(const Eigen::MatrixXd& _contact_array);
 
     std::vector<Eigen::MatrixXd> return_filament_nodes_list() const { return filament_nodes_list; }
     std::vector<Eigen::MatrixXd> return_filament_edges_list() const { return filament_edges_list; }
@@ -24,7 +24,7 @@ public:
     int return_number_of_labels() const { return number_of_labels; }
     double return_volume_fraction() const { return volume_fraction; }
     double return_orientational_order_parameter() const { return orientational_order_parameter; }
-    double return_entanglement() const { return entanglement; }
+    double return_entanglement() const { return local_entanglement; }
     double return_total_entanglement() const { return total_entanglement; }
     int return_number_of_local_contacts() const { return number_of_local_contacts; }
     double return_force_sum() const { return force_sum; }
@@ -33,8 +33,9 @@ public:
 
     Eigen::VectorXi sample_edges_locally(const Eigen::Vector3d& query_point, double R) const;
 
-    Eigen::MatrixXd analyzeLocalVolume(const Eigen::Vector3d& query_point, double R_omega, double rod_radius);
-    Eigen::MatrixXd analyzeLocalVolumeFromPrecomputed(const Eigen::Vector3d& query_point, double R_omega, double rod_radius);
+    Eigen::MatrixXd analyze_local_volume(const Eigen::Vector3d& query_point, double R_omega, double rod_radius);
+    Eigen::MatrixXd analyze_local_volume_from_precomputed(const Eigen::Vector3d& query_point, double R_omega, double rod_radius);    
+    Eigen::MatrixXd analyze_local_volume_over_domain(const Eigen::MatrixX3d& query_points, double R_omega, double rod_radius);
 
     double compute_linking_number_for_edges(const Eigen::VectorXd& e_i, const Eigen::VectorXd& e_j) const;
     void compute_edge_wise_entanglement(const Eigen::MatrixXd& _all_edges, const Eigen::VectorXi& _edge_labels, Eigen::MatrixXd& entanglement_matrix);
@@ -50,7 +51,7 @@ private:
     int number_of_labels;
     double volume_fraction;
     double orientational_order_parameter;
-    double entanglement;
+    double local_entanglement;
     double total_entanglement;
 
     int number_of_local_contacts;
